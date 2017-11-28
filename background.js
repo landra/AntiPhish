558412cfd5981cda2a3fb7163ef1b4296061fe9e
Landen <landen@gmail.com>
2017-11-28 13:02:45 -0700

    users can now disable extension

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
+var isSet = true;
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.active) {
+    if (changeInfo.status == 'complete' && tab.active && isSet) {
         // ...inject 'jquery.min.js' and...
         chrome.tabs.executeScript(tab.id, {
             file: "jquery-3.2.1.min.js",
             allFrames: true,
             runAt: "document_idle"
         });
+
         // ...inject 'content.js' into the active tab's page
         chrome.tabs.executeScript(tab.id, {
             file: "content.js",
@@ -14,3 +16,21 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
         });
     }
 });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Look through all the pages in this extension to find one we can use.
+    if (isSet) {
+        isSet = false;
+        chrome.browserAction.setIcon({
+            path : "off-icon.png"
+        });
+    }
+    else{
+        isSet = true;
+        chrome.browserAction.setIcon({
+            path : "on-icon.png"
+        });
+    }
+});
+
+
